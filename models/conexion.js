3703ea48c7fb81e8bc04093b824e7f130b6ee5fe
cande <candeguinzburg@gmail.com>
2023-12-17 19:37:17 -0300

    first commit

---
+++ models/conexion.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const password =  process.env.PASSWORD
+const CONNECTION_URL =`mongodb+srv://cande2003:${password}@cluster0.wlqaceu.mongodb.net/BienestarSport?retryWrites=true&w=majority`
+
+
+mongoose.connect(CONNECTION_URL,
+   {
+    useNewUrlParser: true,
+  
+   }).then(() =>{
+      console.log('conexion exitosa!')
+  })
+  .catch((err) =>{
+      console.error(err)
+  })
+module.exports = mongoose
